1d23643d99d64f45977208493ec1dfaf5cd5a155
BakaKikuk <101536728+KikukBaka@users.noreply.github.com>
2024-01-05 19:26:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import itertools
-import base64
 import pandas as pd
 import numpy as np
 from imblearn.over_sampling import SMOTE
@@ -11,16 +10,7 @@ import pickle
 with open("data/hungarian.data", encoding='Latin1') as file:
   lines = [line.strip() for line in file]
 
-@st.experimental_memo
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-
-img = get_img_as_base64("image.jpg")
-
-page_bg_img = f"""
+page_bg_img = """
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: url("https://initiate.alphacoders.com/images/122/cropped-1920-1080-1228767.jpg?1155");
@@ -30,20 +20,6 @@ background-repeat: no-repeat;
 background-attachment: local;
 }}
 
-[data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
-background-position: center; 
-background-repeat: no-repeat;
-background-attachment: fixed;
-}}
-
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-
-[data-testid="stToolbar"] {{
-right: 2rem;
-}}
 </style>
 """
 
